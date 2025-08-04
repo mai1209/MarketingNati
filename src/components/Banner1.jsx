49a56b2bdce278959c364d53325a5d8dc1f7b7c9
Mai <maidev@Mac.fibertel.com.ar>
2025-08-04 16:49:11 -0300

    animaciones

--- src/components/Banner1.jsx
+++ src/components/Banner1.jsx
@@ -3,88 +3,76 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 import { useEffect } from "react";
 
-
 function Banner1() {
-  useEffect(() => {
-    AOS.init({
 
-      duration: 1500,
-      offset: 200,    
-      once: true,   
-    });
-  }, []);
 
   return (
-      <div>
-            <div className={style.container_banner}>
-
-
-      <div className={style.blue}>
-        <div className={style.container_all_text}>
-          <div className={style.container_text} data-aos="fade-up">
-            <p className={style.textBlue1}>Crea desde tu <br /> autenticidad</p>
+    <div>
+      <div className={style.container_banner}>
+        <div className={style.blue}>
+          <div className={style.container_all_text}>
+            <div className={style.container_text} >
+              <p className={style.textBlue1}>Crea desde tu <br /> autenticidad</p>
+            </div>
+            <div className={style.container_text}>
+              <p className={style.text2}>Comunicá con <br /> intención</p>
+            </div>
+            <div className={style.container_text} >
+              <p className={style.text3}>Conecta <br /> desde lo humano</p>
+            </div>
           </div>
-          <div className={style.container_text} data-aos="fade-right">
-            <p className={style.text2}>Comunicá con <br /> intención</p>
-          </div>
-          <div className={style.container_text} data-aos="fade-left">
-            <p className={style.text3}>Conecta <br /> desde lo humano</p>
+          <div >
+            <p className={style.bottom_text}>Crece con estrategia, foco y autenticidad</p>
           </div>
         </div>
-        <div data-aos="fade-up">
-          <p className={style.bottom_text}>Crece con estrategia, foco y autenticidad</p>
+
+        <div id="conóceme" className={style.container_text2}>
+          <h2 className={style.h2} >CONOCE MAS DE MI</h2>
+
+          <p className={style.text} >
+            Soy Natalia, asesora en marketing, comunicación y estrategia digital.
+          </p>
+
+          <p className={style.text} >
+            Mi camino comenzó con un emprendimiento propio: un salón de belleza en
+            La Plata. Viajar, explorar culturas y vivir la maternidad durante la
+            pandemia despertaron en mí una nueva vocación: el mundo digital.
+          </p>
+
+          <p className={style.text} >
+            Desde entonces, acompaño a marcas, profesionales y empresas que desean
+            crecer de forma auténtica, estratégica y alineada a su esencia.
+          </p>
+
+          <p className={style.text} >
+            Me especializo en crear planes con foco, claridad, compromiso y una
+            visión realista.
+          </p>
+
+          <p className={style.text} >
+            Creo en el poder de la conexión humana, el trabajo colaborativo y la
+            co-creación con propósito.
+          </p>
+
+          <div className={style.line} data-aos="zoom-in" data-aos-delay="700"></div>
         </div>
       </div>
 
-
-      <div id="conóceme" className={style.container_text2} data-aos="fade-up">
-        <h2 className={style.h2} data-aos="fade-down">CONOCE MAS DE MI</h2>
-
-        <p className={style.text} data-aos="fade-right">
-          Soy Natalia, asesora en marketing, comunicación y estrategia digital.
-        </p>
-
-        <p className={style.text} data-aos="fade-left">
-          Mi camino comenzó con un emprendimiento propio: un salón de belleza en
-          La Plata. Viajar, explorar culturas y vivir la maternidad durante la
-          pandemia despertaron en mí una nueva vocación: el mundo digital.
-        </p>
-
-        <p className={style.text} data-aos="fade-right">
-          Desde entonces, acompaño a marcas, profesionales y empresas que desean
-          crecer de forma auténtica, estratégica y alineada a su esencia.
-        </p>
-
-        <p className={style.text} data-aos="fade-left">
-          Me especializo en crear planes con foco, claridad, compromiso y una
-          visión realista.
-        </p>
-
-        <p className={style.text} data-aos="fade-up">
-          Creo en el poder de la conexión humana, el trabajo colaborativo y la
-          co-creación con propósito.
-        </p>
-
-        <div className={style.line} data-aos="zoom-in"></div>
-      </div>
-
-      </div>
-
-        <div id="enfoque" className={style.background}>
-         <div className={style.container_advice}>
-          <img src="./img/imgAdvice.png" alt="fondoadvice" />
-           <p className={style.text8}>
+      <div id="enfoque" className={style.background}>
+        <div className={style.container_advice} data-aos="fade-up" data-aos-delay="100">
+          <img src="./img/imgAdvice.png" alt="fondoadvice" data-aos="zoom-out" data-aos-delay="200"/>
+          <p className={style.text8} data-aos="fade-up" data-aos-delay="300">
             Acompaño a empresas, emprendedores y freelancers a construir estrategias
             de marketing y comunicación alineadas con su propósito. <br /> Estoy acá para
             ayudarte a ordenar, priorizar y comunicar con claridad, a través de
             procesos estratégicos que respeten la esencia de tu marca.
           </p>
-         </div>
-         <p className={style.p}>
-            Todo adaptado <br /> a vos, con una  <br />mirada <br />   cercana y  <br />personalizada
-          </p>
         </div>
+        <p className={style.p} data-aos="fade-left" data-aos-delay="400">
+          Todo adaptado <br /> a vos, con una <br />mirada <br /> cercana y <br />personalizada
+        </p>
       </div>
+    </div>
   );
 }
 
